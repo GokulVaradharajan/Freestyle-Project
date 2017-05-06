6e7e5011188d7bcbadaf89d4b0b374f59f3ff888
Goudy <gowdilyan@gmail.com>
2017-05-06 16:04:24 -0400

    New commit

--- PrintProgram.bat
+++ PrintProgram.bat
@@ -1,1 +1,1 @@
-echo "Hello-8277 Ohio - Piney Orchard Blacklick - Added GIT - Local Machine - automated build - May05 change start - 0 -"
+echo "Hello-8277 Ohio - Piney Orchard Blacklick - Added GIT - Local Machine - automated build - May05 change start - 0 - 1 -"
